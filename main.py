8aead1f82ad77aabcb167d986d1af3a428ed9837
Evgeniya <osipova.dr17@yandex.ru>
2025-04-11 11:48:57 +1000

    add class DBManager

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from tabulate import tabulate
+
 from config import config  # твои параметры подключения
+from src.db_manager import DBManager
 from src.hh_api import HeadHunter  # твой класс для сбора данных
 from src.utils import create_database, insert_data_to_database  # функции для работы с БД
 
@@ -7,23 +10,33 @@ def main():
     params = config()
     # Название базы данных
     database_name = 'hh_database'
+    db_manager = DBManager(database_name, params)
 
-    # Создание экземпляра класса HeadHunter
-    hh = HeadHunter()
+    def print_table(data, headers):
+        print(tabulate(data, headers=headers, tablefmt='fancy_grid', stralign='center', numalign='center'))
 
-    # Получение данных о работодателях и вакансиях
-    print("Получение данных с HeadHunter...")
-    employers_data = hh.get_employers_with_vacancies()
+    print("\nКомпании и количество вакансий:")
+    companies = db_manager.get_companies_and_vacancies_count()
+    print_table(companies, ["Компания", "Количество вакансий"])
 
-    # Создание базы данных и таблиц
-    print("Создание базы данных и таблиц...")
-    create_database(database_name, params)
+    print("\nВсе вакансии:")
+    vacancies = db_manager.get_all_vacancies()
+    print_table(vacancies, ["Компания", "Вакансия", "Зарплата", "Ссылка"])
 
-    # Загрузка данных в базу данных
-    print("Загрузка данных в базу данных...")
-    insert_data_to_database(employers_data, database_name, params)
+    print("\nСредняя зарплата по вакансиям:")
+    avg_salary = db_manager.get_avg_salary()
+    print(f"{avg_salary:.2f} ₽")
 
-    print("Работа программы завершена успешно!")
+    print("\nВакансии с зарплатой выше средней:")
+    higher_salary_vacancies = db_manager.get_vacancies_with_higher_salary()
+    print_table(higher_salary_vacancies, ["Компания", "Вакансия", "Зарплата", "Ссылка"])
+
+    print("\nВакансии по ключевому слову 'Python':")
+    keyword_vacancies = db_manager.get_vacancies_with_keyword('Python')
+    print_table(keyword_vacancies, ["Компания", "Вакансия", "Зарплата", "Ссылка"])
+
+    db_manager.close()
+
 
 
 if __name__ == '__main__':
